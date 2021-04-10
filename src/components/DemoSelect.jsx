afd6623beb6af2f80d088ae78e6099eb08ea22c5
root <root@DESKTOP-D17D7RC.localdomain>
2021-04-09 21:20:09 -0300

    final version

--- src/components/DemoSelect.jsx
+++ src/components/DemoSelect.jsx
@@ -6,25 +6,28 @@ function DemoSelect() {
 
     const [loading, setLoading] = useState(true)
     const [data, setData] = useState([])
+    const [random, setRandom] = useState(Math.floor(Math.random() * 20))
 
     useEffect(() => {
-        async function fetchMyAPI() {
-            const response = await fetch('https://rickandmortyapi.com/api/character')
-            const data = await response.json()
-            setData(data)
-            setTimeout(() => {
-                setLoading(false)
-            }, 1000)
-        }
         fetchMyAPI()
     }, [])
 
+    const fetchMyAPI = async () => {
+        const response = await fetch('https://rickandmortyapi.com/api/character')
+        const data = await response.json()
+        setData(data.results)
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000)
+    }
+
     return (
         <div className='contenedor'>
+            {console.log('cargo')}
             {loading ? (
                 <Loading />
             ) : (
-                <Select initialValue={data.results[0]} data={data.results} />
+                <Select initialValue={data[random]} data={data} />
             )}
         </div>
     )
--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -5,11 +5,11 @@ import './Select.css'
 function Select({ initialValue, data }) {
 
     const [selected, setSelected] = useState(initialValue)
-    const [disabled, setDisabled] = useState('')
+    const [disabled, setDisabled] = useState(false)
 
     useEffect(() => {
-        if (data.length > 1) {
-            setDisabled('Disabled ')
+        if (data.length <= 1) {
+            setDisabled(true)
         }
     }, [])
 
@@ -17,16 +17,19 @@ function Select({ initialValue, data }) {
         <div className='row'>
             <div className='col'>
                 <select
-                    className={`form-select form-control mb-3 ${disabled}`}
+                    disabled={disabled}
+                    className={`form-select form-control mb-3 ${disabled&&('bg-secondary')}`}
                     aria-label='Default select example'
                     onChange={(e) => (setSelected(data[e.target.value]))}>
+                        <option key={initialValue.id} value={data.indexOf(initialValue)} label={initialValue.name} />
                     {data.map((item, index) => {
+                        if(item !== initialValue) {
                         return (
                             <option key={item.id} value={index} label={item.name} />
-                        )
+                        )}
                     })}
                 </select>
-                <Card character={selected} />
+                {data.length===0 ? (<h3 style={{'color': '#fafafa'}}>No hay resultados para mostrar 💩</h3>) : (<Card character={selected} />)}
             </div>
         </div>
     )
